54ce66676b0235c5155f79201682aaaae9defc18
Daniel James Prohasky <daniel.prohasky@rmit.edu.au>
2018-10-19 15:56:34 +1100

    claw sync

--- FIRMWARE/testCode/stepperSerial/stepperSerial.ino
+++ FIRMWARE/testCode/stepperSerial/stepperSerial.ino
@@ -17,24 +17,23 @@
 
 #include <Stepper.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+const int stepsPerRevolution = 200 * 16; // change this to fit the number of steps per revolution
 // for your motor
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 3, 4);
+Stepper myStepper(stepsPerRevolution, A0, A1);
 
 // read serial
 char c;
 
 void setup() {
   // set the speed at 60 rpm:
-  myStepper.setSpeed(20);
+  myStepper.setSpeed(80);
   // initialize the serial port:
   Serial.begin(9600);
 }
 
 void loop() {
-
   if (Serial.available()) {
     c = Serial.read();
     Serial.println(c);
